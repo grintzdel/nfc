9a72158897aa69518846eb05431af3c59ef2956a
maoudin <mathis.oudin42@gmail.com>
2026-05-13 10:40:03 +0200

    fix: ci e2e

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -30,6 +30,6 @@ export default defineConfig({
     command: 'cd .. && pnpm dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
-    timeout: 60_000,
+    timeout: 120_000,
   },
 })
